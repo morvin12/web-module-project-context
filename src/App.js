6d5ce2e5bca917d01fd5ce17f4a6c9374089dd0b
Michael Orvin <morvin1212@gmail.com>
2021-12-09 20:00:00 -0700

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+
+import ProductContext from './contexts/ProductContexts';
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -17,16 +20,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<Navigation/>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Route exact path="/">
+						<Products/>
+					</Route>
+				</ProductContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart/>
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
